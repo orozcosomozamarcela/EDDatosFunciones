db1906965abb7e7e278b5cf335086ce61f1714ce
Marcela Orozco <orozcosomozamarcela@gmail.com>
2020-10-01 13:57:48 -0300

    tarea 5 no terminada

---
+++ orozco_tarea5_funciones/tarea5_orozco/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tarea5_orozco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hola");
+        }
+    }
+}
